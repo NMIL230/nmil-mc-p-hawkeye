7db98659ebba0dd82763dc20156ec8ea76902818
BuildTools <unconfigured@null.spigotmc.org>
2024-01-16 13:14:19 -0600

    build master v0.0.2

--- src/main/java/nmil/mceeg/plugin/buildmaster/BuildMaster.java
+++ src/main/java/nmil/mceeg/plugin/buildmaster/BuildMaster.java
@@ -4,38 +4,107 @@ import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.World;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.event.Listener;
 
-public class BuildMaster extends JavaPlugin {
-    Location lobby_location;
-    Location platform_location;
-    Location[] difficultyLever_locations = new Location[10];
+public class BuildMaster extends JavaPlugin implements Listener {
+    private Location lobby_location;
+    private Location platform_location;
+    private Location[] difficultyLever_locations;
+    private Location saveButton_location;
+    private Location tpLobbyButton_location;
+
+    private int difficulty;
 
     private void getLocations() {
         World world = Bukkit.getWorld("Gelazkor");
         if (world != null) {
-            lobby_location = new Location(world, 3772, 123, 4166, 90, 0);
-            platform_location = new Location(world, 2614, 141, 2462, -90, 0);
+            setLobby_location(new Location(world, 3772, 123, 4166, 90, 0));
+            setPlatform_location(new Location(world, 2614, 141, 2462, -90, 0));
 
-            int x = 3763;
-            int y = 125;
-            int z = 4171;
-            for (int i = 0; i < difficultyLever_locations.length; i++) {
-                difficultyLever_locations[i] = new Location(world, x, y, z - i, 0, 0);
+            setSaveButton_location(new Location(world, 2604, 142, 2456));
+            setTpLobbyButton_location(new Location(world, 2604, 142, 2457));
+
+            int x = 3763; int y = 125; int z = 4171;
+            Location[] locations = new Location[10];
+            for (int i = 0; i < locations.length; i++) {
+                locations[i] = new Location(world, x, y, z - i, 0, 0);
             }
+            setDifficultyLever_locations(locations);
+            getLogger().info("getLocations loaded: " + lobby_location + platform_location);
+
         }
+
     }
 
 
     @Override
     public void onEnable() {
         getLocations();
-
+//        getServer().getPluginManager().registerEvents(this, this);
         getServer().getPluginManager().registerEvents(new BlockBreakListener(), this);
-        getServer().getPluginManager().registerEvents(new PlayerSpawnListener(), this);
+        getServer().getPluginManager().registerEvents(new PlayerSpawnListener(lobby_location), this);
+        getServer().getPluginManager().registerEvents(new InteractionListener(this), this);
 
         getLogger().info("BuildMaster loaded!");
 
     }
+    @Override
+    public void onDisable() {
 
-    // onDisable()
+     }
+
+
+
+
+
+
+    public Location getLobby_location() {
+        return lobby_location;
+    }
+
+    public void setLobby_location(Location lobby_location) {
+        this.lobby_location = lobby_location;
+    }
+
+    public Location getPlatform_location() {
+        return platform_location;
+    }
+
+    public void setPlatform_location(Location platform_location) {
+        this.platform_location = platform_location;
+    }
+
+    public Location[] getDifficultyLever_locations() {
+        return difficultyLever_locations;
+    }
+
+    public void setDifficultyLever_locations(Location[] difficultyLever_locations) {
+        this.difficultyLever_locations = difficultyLever_locations;
+    }
+
+    public int getDifficulty() {
+        return difficulty;
+    }
+
+    public void setDifficulty(int difficulty) {
+        this.difficulty = difficulty;
+    }
+
+    public Location getSaveButton_location() {
+        return saveButton_location;
+    }
+
+    public void setSaveButton_location(Location saveButton_location) {
+        this.saveButton_location = saveButton_location;
+    }
+
+    public Location getTpLobbyButton_location() {
+        return tpLobbyButton_location;
+    }
+
+    public void setTpLobbyButton_location(Location tpLobbyButton_location) {
+        this.tpLobbyButton_location = tpLobbyButton_location;
+    }
+
+
 }
--- src/main/java/nmil/mceeg/plugin/buildmaster/PlayerSpawnListener.java
+++ src/main/java/nmil/mceeg/plugin/buildmaster/PlayerSpawnListener.java
@@ -1,20 +1,29 @@
 package nmil.mceeg.plugin.buildmaster;
 
-import org.bukkit.Location;
-import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.EventHandler;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerRespawnEvent;
+import org.bukkit.Location;
+import org.bukkit.entity.Player;
 
 public class PlayerSpawnListener implements Listener {
+    private Location spawnLocation;
 
-    @EventHandler
-    public void onPlayerJoin(PlayerJoinEvent event, Location spawnLocation) {
-        event.getPlayer().setBedSpawnLocation(spawnLocation, true);
+    public PlayerSpawnListener(Location spawnLocation) {
+        this.spawnLocation = spawnLocation;
     }
 
     @EventHandler
-    public void onPlayerRespawn(PlayerRespawnEvent event, Location respawnLocation) {
-        event.setRespawnLocation(respawnLocation);
+    public void onPlayerJoin(PlayerJoinEvent event) {
+        Player player = event.getPlayer();
+        if (!player.hasPlayedBefore()) {
+            player.teleport(spawnLocation);
+        }
+    }
+
+    @EventHandler
+    public void onPlayerRespawn(PlayerRespawnEvent event) {
+        event.setRespawnLocation(spawnLocation);
     }
 }
