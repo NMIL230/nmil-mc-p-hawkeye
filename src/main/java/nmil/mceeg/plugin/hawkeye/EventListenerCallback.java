599d98f51dce7b3ab5401057b0a90f37a5c75e2d
BuildTools <unconfigured@null.spigotmc.org>
2024-01-23 16:38:04 -0600

    BuildMaster with Hawkeye v0.3.0

--- src/main/java/nmil/mceeg/plugin/hawkeye/EventListenerCallback.java
+++
@@ -1,6 +0,0 @@
-package nmil.mceeg.plugin.hawkeye;
-import org.bukkit.entity.Player;
-
-public interface EventListenerCallback {
-    void handleEventInfo(Player player, String eventInfo, String type);
-}
